75256e148e7eff39b9052344f1365f617c0b414e
Rajeev SD <rajeevratnasd@gmail.com>
2021-04-21 11:09:11 +0530

    First commit

---
+++ src/main/java/com/myspring/allanno/AllannoApplication.java
@@ -0,0 +1,13 @@
+package com.myspring.allanno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllannoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllannoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myspring/allanno/AllannoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspring.allanno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllannoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
